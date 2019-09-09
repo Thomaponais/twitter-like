2412ba1295e0b383eb1877e44adad6a58094085c
Thomaponais <thomaponais@gmail.com>
2019-09-09 12:01:25 +0900

    Created Tweets and answers tab

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,10 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-window.setTimeout(function() {
-  $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
+$(document).on('turbolinks:load', function() {
+  window.setTimeout(function() {
+    $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
       $(this).remove();
-  });
-}, 3000);
+    });
+  }, 3000);
+});
